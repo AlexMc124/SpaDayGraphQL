c43eeabc7d744b6e77bd9933fd8bcaee9e085514
Alex McIntosh <alex.mcintosh124@gmail.com>
2022-04-01 12:51:10 +0100

    set up graphql and routes

---
+++ books/schema.py
@@ -0,0 +1,14 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Books
+
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Books
+        fields = ('id', 'title', 'excerpt')
+
+class Query(graphene.ObjectType):
+    books = graphene.List(BookType)
+
+schema = graphene.Schema(query=Query)
