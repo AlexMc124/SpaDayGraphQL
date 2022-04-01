c2f00ce40c05aa256f4e62fe9cb963e7da3c6a38
Alex McIntosh <alex.mcintosh124@gmail.com>
2022-04-01 14:43:47 +0100

    Query All Books Works

--- books/schema.py
+++ books/schema.py
@@ -3,12 +3,17 @@ from graphene_django import DjangoObjectType
 from .models import Books
 
 
-class BookType(DjangoObjectType):
+class BooksType(DjangoObjectType):
     class Meta:
         model = Books
         fields = ('id', 'title', 'excerpt')
 
 class Query(graphene.ObjectType):
-    all_books = graphene.List(BookType)
+
+    all_books = graphene.List(BooksType)
+
+    def resolve_all_books(root, info):
+        return Books.objects.all()
+
 
 schema = graphene.Schema(query=Query)
